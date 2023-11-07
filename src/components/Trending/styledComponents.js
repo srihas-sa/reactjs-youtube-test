afd09944c100b5e1cdd1208c06df56248da72f99
srihas-sa <edupugantisrihas262003@gmail.com>
2023-11-07 20:22:14 +0000

    Updates

--- src/components/Trending/styledComponents.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const HomeOuterMostContainer = styled.div`
-  color: ${props => (props.outline ? 'black' : 'white')};
-  font-family: 'Roboto';
-  background-color: ${props => (props.outline ? '#0f0f0f ' : '#f9f9f9')};
-`
